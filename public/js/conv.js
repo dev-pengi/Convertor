49b678e77ad8b69ca4db70c00d162c5f8950b340
penguin-9999 <siffffdoul@gmail.com>
2022-12-11 06:10:32 +0100

    update

--- public/js/conv.js
+++ public/js/conv.js
@@ -7,23 +7,28 @@ document.querySelector('#units_type').querySelector('.select-dropdown__list').in
 }).join('');
 selects();
 
-
-
-document.querySelector('#units_type').addEventListener('click', () => {
-  const unit = units.find(obj => obj.value == document.querySelector('#units_type').getAttribute('data-value'))
-  if (!unit) return
-  let html = () => {
-    return unit.data.map(unit => {
-      return `
+let currentType = null;
+units_refresh();
+function units_refresh() {
+  document.querySelector('#units_type').addEventListener('click', () => {
+    if (document.querySelector('#units_type').getAttribute('data-value') == currentType) return
+    const unit = units.find(obj => obj.value == document.querySelector('#units_type').getAttribute('data-value'))
+    if (!unit) return
+    let html = () => {
+      return unit.data.map(unit => {
+        return `
       <li data-value="${unit.value}" class="select-item">
         ${unit.name}
       </li>`
-    }).join('');
-  }
-  document.querySelector('#from_unit').querySelector('.select-dropdown__list').innerHTML = html()
-  document.querySelector('#to_unit').querySelector('.select-dropdown__list').innerHTML = html()
-  selects();
-})
+      }).join('');
+    }
+    document.querySelector('#from_unit').querySelector('.select-dropdown__list').innerHTML = html();
+    document.querySelector('#to_unit').querySelector('.select-dropdown__list').innerHTML = html();
+    currentType = document.querySelector('#units_type').getAttribute('data-value');
+    selects();
+    units_refresh();
+  })
+}
 
 const amount = document.querySelector('#amount')
 const result = document.querySelector('#result')
@@ -35,14 +40,11 @@ document.querySelector('#convert').addEventListener('click', () => {
   reset_err()
   if (!amount.value.trim().length) return write_err('Please enter the amount')
   if (isNaN(amount.value)) return write_err('Please enter a valid number to convert')
-  if (amount.value < 0) return write_err('Please use a positive amount')
   const unit = units.find(obj => obj.value == document.querySelector('#units_type').getAttribute('data-value'))
   if (!unit) return write_err('Please selects units')
   const from_unit = unit.data.find(obj => obj.value == document.querySelector('#from_unit').getAttribute('data-value'))
-  console.log(from_unit);
   if (!from_unit) return write_err('Please choose what unit you want to convert')
   const to_unit = unit.data.find(obj => obj.value == document.querySelector('#to_unit').getAttribute('data-value'))
-  console.log(to_unit);
   if (!to_unit) return write_err('Please choose what unit you want to convert to')
 
   const convert = (from_unit.divide * amount.value) / to_unit.divide
--- public/js/vars.js
+++ public/js/vars.js
@@ -1,50 +1,171 @@
 let units = [
-    {
-      name: 'time',
-      value: 0,
-      data: [
-        {
-          name: 'milliseconds',
-          value: 0,
-          divide: .001
-        },
-        {
-          name: 'seconds',
-          value: 1,
-          divide: 1
-        },
-        {
-          name: 'minutes',
-          value: 2,
-          divide: 60
-        },
-        {
-          name: 'hours',
-          value: 3,
-          divide: 3600
-        },
-        {
-          name: 'days',
-          value: 4,
-          divide: 86400
-        },
-        {
-          name: 'weeks',
-          value: 5,
-          divide: 604800
-        },
-        {
-          name: 'months',
-          value: 6,
-          divide: 2592000
-        },
-        {
-          name: 'year',
-          value: 7,
-          divide: 31104000
-        }
-      ]
-    }
-  ]
-  
-  
+  {
+    name: 'time',
+    value: 0,
+    data: [
+      {
+        name: 'milliseconds',
+        value: 0,
+        divide: .001
+      },
+      {
+        name: 'seconds',
+        value: 1,
+        divide: 1
+      },
+      {
+        name: 'minutes',
+        value: 2,
+        divide: 60
+      },
+      {
+        name: 'hours',
+        value: 3,
+        divide: 3600
+      },
+      {
+        name: 'days',
+        value: 4,
+        divide: 86400
+      },
+      {
+        name: 'weeks',
+        value: 5,
+        divide: 604800
+      },
+      {
+        name: 'months',
+        value: 6,
+        divide: 2592000
+      },
+      {
+        name: 'year',
+        value: 7,
+        divide: 31104000
+      }
+    ]
+  },
+  {
+    name: 'length',
+    value: 1,
+    data: [
+      {
+        name: 'nanometer',
+        value: 0,
+        divide: 0.000000001
+      },
+      {
+        name: 'micrometer',
+        value: 1,
+        divide: 0.000001
+      },
+      {
+        name: 'millimeter',
+        value: 2,
+        divide: 0.001
+      },
+      {
+        name: 'centimeter',
+        value: 3,
+        divide: 0.01
+      },
+      {
+        name: 'meter',
+        value: 4,
+        divide: 1
+      },
+      {
+        name: 'kilometer',
+        value: 5,
+        divide: 1000
+      },
+      {
+        name: 'mile',
+        value: 6,
+        divide: 1609.35
+      },
+      {
+        name: 'Inch',
+        value: 7,
+        divide: 0.0254
+      },
+      {
+        name: 'foot',
+        value: 8,
+        divide: 0.3048
+      },
+      {
+        name: 'yard',
+        value: 9,
+        divide: 0.9144
+      },
+      {
+        name: 'Light Year',
+        value: 10,
+        divide: 9460660000000000
+      },
+    ]
+  },
+  {
+    name: 'area',
+    value: 2,
+    data: [
+      {
+        name: 'square micrometer',
+        value: 0,
+        divide: 0.000000000001
+      },
+      {
+        name: 'square millimeter',
+        value: 1,
+        divide: 0.000001
+      },
+      {
+        name: 'square centimeter',
+        value: 2,
+        divide: 0.0001
+      },
+      {
+        name: 'square meter',
+        value: 3,
+        divide: 1
+      },
+      {
+        name: 'square kilometer',
+        value: 4,
+        divide: 1000000
+      },
+      {
+        name: 'hectare',
+        value: 5,
+        divide: 10000
+      },
+      {
+        name: 'square mile',
+        value: 6,
+        divide: 2589990
+      },
+      {
+        name: 'square yard',
+        value: 7,
+        divide: 0.83612736
+      },
+      {
+        name: 'square foot',
+        value: 8,
+        divide: 0.09290304
+      },
+      {
+        name: 'square inch',
+        value: 9,
+        divide: 0.00064516
+      },
+      {
+        name: 'acre',
+        value: 10,
+        divide: 4046.8564224
+      },
+    ]
+  }
+]
+
